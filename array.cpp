d79608501af0008a23b50dbb135d1321e23620a5
Abhijit Bhattacharjee <abhijit@Abhijits-MacBook-Air.local>
2025-11-21 00:01:34 +0530

    updated

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>//array x=13 
+
+int main(){
+    int arr[6] = {1,11,5,4,2,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int temp; 
+    int result = 13;
+
+    for(int i=0; i < n;i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] + arr[j] == result){
+                printf("Pair found: (%d, %d)\n", arr[i], arr[j]);
+                return 0;
+            }
+        }
+    }
+    return 0;
+
+}
--- something.c
+++ something.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main(){
-    int age = 2;
+    int n;
+    scanf("%d", &n);
+
+    if(n == 0){
+        printf(0);
+
+    } else if (n == 1) {
+        printf(1);
+    }
+    else {
+        printf("%d", fibonacci(n));
+    }
+
+
+    return 0;
 }
