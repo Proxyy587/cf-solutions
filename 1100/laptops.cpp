24ea552358f2c12da930aac684903e986718af45
Abhijit Bhattacharjee <abhijitbhattacharjee333@gmail.com>
2026-01-24 02:18:07 +0530

    1100 laptops

--- 1100/laptops.cpp
+++ 1100/laptops.cpp
@@ -18,7 +18,7 @@ int main() {
    
    for(int i=0; i < n; i++){
        for(int j=0; j < 1; j++){
-        //   if 2 > 1 or 1 < 2
+        //   if 2 > 1 or 1x  < 2
            if(laptop[i][j] < laptop[i][j+1]){
                happy = true;
            } else break;
